188e1a1ff7382003f6c89d98e9fe933a756db90d
akramsadat78 <safavimoghadam.78@gmail.comgit config --global user.name Your>
2020-04-04 18:38:38 +0430

    order  index.js

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+/**********************require**************************/
 const express = require('express')
 const app = express()
 
 const dotenv= require("dotenv");
 dotenv.config();
 
+const fs = require('fs');
+
+var json = require('./data.json');
+
+var geojsonMerge = require('@mapbox/geojson-merge');
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+/**********************get**************************/
 app.get('/gis/testpoint?lat=:id,long=:id', (req, res) => 
     res.json(
         {
@@ -16,34 +28,21 @@ app.get('/gis/testpoint?lat=:id,long=:id', (req, res) =>
     )
        )
 
-
-const fs = require('fs');
-const bodyParser = require('body-parser')
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-var json = require('./data.json');
-var geojsonMerge = require('@mapbox/geojson-merge');
+/**********************put**************************/
 app.put('/gis/addpolygon', function (req, res) {
   
-   // console.log(json);
-    let da = JSON.stringify(req.body, null, 2);
-
+    let data = JSON.stringify(req.body, null, 2);
 
     var newGeoJSON = { 
         "type" : "FeatureCollection",
-        "features": [... json.features, da]
+        "features": [... json.features, data]
     }
 
-    console.log(newGeoJSON);
+    data = JSON.stringify(newGeoJSON, null, 2);
 
-    let d = JSON.stringify(newGeoJSON, null, 2);
-
-    fs.writeFileSync('data.json', d);
-
-  res.send('Got a PUT request at /user');
+    fs.writeFileSync('data.json', data);
+    
+    res.send('Got a PUT request at /user');
 })
- 
 
 app.listen(process.env.port, () => console.log(`Example app listening on port ${process.env.port}!`))
