085032da6531a90428005ac5fd9b6e5475481471
akramsadat78 <safavimoghadam.78@gmail.comgit config --global user.name Your>
2020-04-04 18:11:35 +0430

    send data from put to json file

--- index.js
+++ index.js
@@ -16,4 +16,34 @@ app.get('/gis/testpoint?lat=:id,long=:id', (req, res) =>
     )
        )
 
+
+const fs = require('fs');
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+var json = require('./data.json');
+var geojsonMerge = require('@mapbox/geojson-merge');
+app.put('/gis/addpolygon', function (req, res) {
+  
+   // console.log(json);
+    let da = JSON.stringify(req.body, null, 2);
+
+
+    var newGeoJSON = { 
+        "type" : "FeatureCollection",
+        "features": [... json.features, da]
+    }
+
+    console.log(newGeoJSON);
+
+    let d = JSON.stringify(newGeoJSON, null, 2);
+
+    fs.writeFileSync('data.json', d);
+
+  res.send('Got a PUT request at /user');
+})
+ 
+
 app.listen(process.env.port, () => console.log(`Example app listening on port ${process.env.port}!`))
