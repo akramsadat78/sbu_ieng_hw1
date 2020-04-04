0094ba202708d9c6f00535f3758877022547402f
akramsadat78 <safavimoghadam.78@gmail.comgit config --global user.name Your>
2020-04-04 23:27:10 +0430

    check existance of point in polygons

--- index.js
+++ index.js
@@ -15,35 +15,52 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+const isValidCoordinates = require('is-valid-coordinates')
+
+var inside = require('point-in-polygon')
+
 /**********************get**************************/
-app.get('/gis/testpoint?lat=:id,long=:id', (req, res) => 
-    res.json(
-        {
-            polygons : [
-                'استان تهران' ,
-                 'شهر تهران' , 
-                 'منطقه ۳'
-            ]
+app. get('/gis/testpoint?lat=:id_lat,long=:id_long', function (req, res) {
+     
+    var id_lat = req.params.id_lat;
+    var id_long = req.params.id_long;
+
+    inside_polygon=false;
+
+    //check existance of point in polygons 
+    for (var i = 0;i<json.features.length; i++) {
+        if( inside([ id_lat, id_long ], json.features[i].geometry.coordinates[0]) === true){
+
+            inside_polygon=true;
+            res.json(200);
+
         }
-    )
-       )
+       
+    }
+
+    if( inside_polygon===false ){
+        res.json(400);
+    }
+
+
+})
 
 /**********************put**************************/
-//et port = process.env.port;
-port =3000;
+let port = process.env.port;
+
 app.put('/gis/addpolygon', function (req, res) {
   
-    let data = JSON.stringify(req.body, null, 2);
-
+    let data = req.body;
+ 
     var newGeoJSON = { 
         "type" : "FeatureCollection",
         "features": [... json.features, data]
     }
 
     data = JSON.stringify(newGeoJSON, null, 2);
-
-    fs.writeFileSync('data.json', data);
     
+    fs.writeFileSync('data.json', data);
+  
     res.send('Got a PUT request at /user');
 })
 
